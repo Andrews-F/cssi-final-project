4935603cc490b08b84472af8fd39a79618052e11
Demo User <demouser@MacBook-Air.local>
2015-08-03 09:46:46 -0400

    Update main.py

--- main.py
+++ main.py
@@ -22,6 +22,8 @@
 # To clear the datastore:
 # /usr/local/google_appengine/dev_appserver.py --clear_datastore=1 [path_to_app_name]
 
+#Sofie will see this comment and delete it
+
 import os
 import webapp2
 import jinja2
@@ -32,12 +34,12 @@ JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
-    
-    
+
+
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         self.response.write("Hello Word)
-    
+
 
 
 app = webapp2.WSGIApplication([
