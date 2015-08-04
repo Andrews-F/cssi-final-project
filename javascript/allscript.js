60011d9eff5bb000c5896cf9d7570929a8cfa1d4
Sofie Cornelis <sacornelis97@gmail.com>
2015-08-04 14:55:39 -0400

    tying to fix javascript on home page

--- javascript/allscript.js
+++ javascript/allscript.js
@@ -1,21 +1,40 @@
 $(document).ready(function() {
-    $("body").css("display", "none");
 
-    $("body").fadeIn(2000);
 
-    $("a.transition").click(function(event){
-        event.preventDefault();
-        linkLocation = this.href;
-        $("body").fadeOut(1000, redirectPage);
-    });
+    $(".mathbubbles").hide();
+    $(".sciencebubbles").hide();
+    $(".historybubbles").hide();
+    $(".csbubbles").hide();
 
-    function redirectPage() {
-        window.location = linkLocation;
-    }
+
+    // $("#Math").click(function () {
+    //   $("#algebracircle").fadeIn( 2000 )
+    //   $("#geometrycircle").fadeIn( 2000 )
+    //   $("#trigcircle").fadeIn( 2000 )
+    //  });
+
+    // $("body").css("display", "none");
+    //
+    // $("body").fadeIn(2000);
+    //
+    // $("a.transition").click(function(event){
+    //     event.preventDefault();
+    //     linkLocation = this.href;
+    //     $("body").fadeOut(1000, redirectPage);
+    // });
+
+
+    // $(function(){
+    //   function fade() {
+    //     $("#MainIsmsageB1").fadeOut(6000);
+    //   }
+    //     $("#MainImsageB1").hover(fade);
+
+
+
+
 });
-$(function(){
-  function fade() {
-    $("#MainIsmsageB1").fadeOut(6000);
-  }
-    $("#MainImsageB1").hover(fade);
-});
+
+// function redirectPage() {
+//     window.location = linkLocation;
+// }
